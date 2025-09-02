a57f1967620753a4373fec972c93358c7e017691
Adrian Arcenas <arcena2@unlv.nevada.edu>
2025-09-02 16:44:13 -0700

    My solution for Project 1 on xv6.

---
+++ user/hello.c
@@ -0,0 +1,8 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(){
+    printf("Hello world, this is Adrian Arcenas.\n");
+    printf("I am in CS370, Fall 2025, Section 1002\n\n");
+}
